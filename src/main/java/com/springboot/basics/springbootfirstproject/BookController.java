aa888cdf097110146d157885f3e3c6441907023c
Swati_Rustogi <swati.rustogi@gmail.com>
2018-11-14 09:17:40 +0530

    Springboot web project - Restcontroller

---
+++ src/main/java/com/springboot/basics/springbootfirstproject/BookController.java
@@ -0,0 +1,24 @@
+package com.springboot.basics.springbootfirstproject;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookController {
+	
+	@GetMapping("/books")
+	public List<Book> getBooks()
+	{
+		List l = new ArrayList<Book>();
+		Book b1 = new Book(1,"Java","abc");
+		Book b2 = new Book(2,"UNIX","xyz");
+		l.add(b1);
+		l.add(b2);
+		
+		return l;
+		
+	}
+	
+}
--- src/main/java/com/springboot/basics/springbootfirstproject/SpringbootFirstProjectApplication.java
+++ src/main/java/com/springboot/basics/springbootfirstproject/SpringbootFirstProjectApplication.java
@@ -2,11 +2,14 @@ package com.springboot.basics.springbootfirstproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringbootFirstProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootFirstProjectApplication.class, args);
+		ApplicationContext appContext = SpringApplication.run(SpringbootFirstProjectApplication.class, args);
+		for(String s : appContext.getBeanDefinitionNames())
+			System.out.println(s);
 	}
 }
